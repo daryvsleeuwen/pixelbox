a6c54f85dd71ecb4dd86e67c4abc67eca905e575
daryvsleeuwen <i410773@fhict.nl>
2020-11-03 16:31:51 +0100

    path follow update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include "FastLED.h"
-
 #if FASTLED_VERSION < 3001000
 #error "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
 #define DATA_PIN 7
-//#define CLK_PIN   4
+
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 #define NUM_LEDS 49
@@ -15,30 +14,62 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS 255
 #define FRAMES_PER_SECOND 120
 
-int genHue = 0;
-bool even = false;
-int lastusedpixel = rand();
+struct pixelCoords
+{
+  int x;
+  int y;
+};
+
+int mainHue = 0;
+struct pixelCoords lastpixel;
+struct pixelCoords newpixel;
+int currentpixels[NUM_LEDS];
+int fadeduration = 1200;
 
 void setup()
 {
   delay(500);
-  Serial.begin(38400);
+  Serial.begin(115200);
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  Serial.println(lastusedpixel);
+  srand(millis());
+  lastpixel = {4, 4};
 }
 
-int pickRandomPixel()
+int convertPixelCoords(struct pixelCoords coords)
 {
-  int pixel = rand() % NUM_LEDS;
+  return coords.x + (coords.y * ROW_LENGTH);
+}
 
-  if (pixel == lastusedpixel)
+// int pickRandomPixel()
+// {
+//   int lastusedpixel = random16(NUM_LEDS);
+//   return lastusedpixel;
+// }
+
+struct pixelCoords findPixelNeighbour(struct pixelCoords coords)
+{
+  srand(millis());
+  int xAddition = (rand() % 3) - 1;
+  int yAddition = (rand() % 3) - 1;
+
+  if (xAddition == 0 && yAddition == 0)
   {
-    pickRandomPixel();
+    findPixelNeighbour(coords);
   }
+  if (coords.x + xAddition > ROW_LENGTH - 1 || coords.y + yAddition > ROW_LENGTH - 1 || coords.x + xAddition < 0 || coords.y + yAddition < 0)
+  {
+    findPixelNeighbour(coords);
+  }
+  else
+  {
+    coords.x += xAddition;
+    coords.y += yAddition;
 
-  return pixel;
+    lastpixel = coords;
+    return lastpixel;
+  }
 }
 
 void loop()
@@ -46,10 +77,17 @@ void loop()
   FastLED.delay(1000 / FRAMES_PER_SECOND);
   FastLED.clear();
 
-  int r = rand() % 255;
-  int g = rand() % 255;
-  int b = rand() % 255;
-
-  leds[pickRandomPixel()] = CRGB(r, g, b);
+  EVERY_N_MILLIS(20)
+  {
+    mainHue += 30;
+    if (mainHue > 255)
+    {
+      mainHue = 0;
+    }
+  }
+  struct pixelCoords coords = findPixelNeighbour(lastpixel);
+  int index = convertPixelCoords(coords);
+  leds[index] = CHSV(mainHue, 255, 255);
   FastLED.show();
+  delay(90);
 }
